0ad2b9febc7b487ee48226fcfa5911fa6ffc9f03
unknown <nbryson2000@gmail.com>
2023-08-16 15:41:01 -0500

    changed metadata again

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'ECGXMLreader',
+    name = 'ECGXML',
     version = '0.1.1',
     packages = find_packages(),
     instal_requires = [
