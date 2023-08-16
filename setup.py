d402ca7e8821a8b7bbcf7323ad1e093ebf59e661
unknown <nbryson2000@gmail.com>
2023-08-16 16:20:59 -0500

    changed to setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'ECGXML',
     version = '0.1.2',
-    packages = find_packages(),
+    packages = ['ECGXMLReader'],
     instal_requires = [
         'xmltodict',
         'numpy'
