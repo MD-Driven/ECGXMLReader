10ce0ef833d93d28cd17d490e0f128073ce335c4
unknown <nbryson2000@gmail.com>
2024-02-13 09:11:53 -0600

    pushing mdd desktop changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'ECGXML',
     version = '0.1.2',
-    packages = ['ECGXMLReader'],
+    packages = find_packages(),
     instal_requires = [
         'xmltodict',
         'numpy'
